f2dc4535790298ef4819697f6ef072e5d0bbfaba
Matteo Griso <m.griso@hotmail.it>
2019-07-12 22:23:28 -0300

    Andon branch creation

---
+++ KisWebApp/Areas/Andon/AndonAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace KIS.Areas.Andon
+{
+    public class AndonAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Andon";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Andon_default",
+                "Andon/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
