45bebaefba8f2a01e6c39366da48bc41c72936b8
Matteo Griso <m.griso@hotmail.it>
2020-11-26 21:55:12 -0300

    Added email scope to authentication Created vcmain database Created accounts table

---
+++ KisWebApp/Areas/AccountsMgm/AccountsMgmAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace KIS.Areas.AccountsMgm
+{
+    public class AccountsMgmAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AccountsMgm";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AccountsMgm_default",
+                "AccountsMgm/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
